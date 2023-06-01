7c8e40cec6a2e0b60f44c12d043690f26ba604c1
Y11o <Y11o@users.noreply.github.com>
2023-06-01 03:59:52 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ChatClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1003, 550);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(1021, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Введите имя";
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(1022, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Принять";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(12, 577);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(1003, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(1032, 559);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 38);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Отправить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1178, 609);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Chat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+
+namespace ChatClient
+{
+    public partial class Form1 : Form
+    {
+        
+
+    public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string host = "192.168.0.101";
+        int port = 8888;
+        TcpClient client = new TcpClient();
+        string userName = "UserName";
+        bool entered = true;
+        StreamReader Reader = null;
+        StreamWriter Writer = null;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "Введите свое имя";
+            textBox1.ForeColor = Color.Gray;
+            listBox1.BeginUpdate();
+            listBox1.Items.Add("Введите свое имя");
+            listBox1.EndUpdate();
+        }
+
+
+
+        private void createConnection() {
+            try
+            {
+                client.Connect(host, port); //подключение клиента
+                Reader = new StreamReader(client.GetStream());
+                Writer = new StreamWriter(client.GetStream());
+                if (Writer is null || Reader is null) return;
+                SendMessageAsync(Writer);
+                // запускаем новый поток для получения данных
+                ReceiveMessageAsync(Reader);
+            }
+            catch (Exception ex)
+            {
+                listBox1.BeginUpdate();
+                listBox1.Items.Add(ex.Message);
+                listBox1.EndUpdate();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            button1.Enabled = false;
+            userName = textBox1.Text;
+            listBox1.BeginUpdate();
+            listBox1.Items.Add($"Добро пожаловать, {userName}!");
+            createConnection();
+            textBox2.Enabled = true;
+            listBox1.EndUpdate();
+            textBox2.Enabled = true;            
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            textBox1.ForeColor = Color.Black;
+        }
+        async Task SendMessageAsync(StreamWriter writer)
+        {
+            // сначала отправляем имя
+            if (entered) {
+                await writer.WriteLineAsync(userName);
+                await writer.FlushAsync();
+                entered = false;
+            }            
+            string message = textBox2.Text;
+            textBox2.Text = "";
+            textBox2.Focus();
+            if (message != null && message != "") {
+                listBox1.BeginUpdate();
+                listBox1.Items.Add($"Вы: {message}");
+                listBox1.EndUpdate();
+                await writer.WriteLineAsync(message);
+                await writer.FlushAsync();
+            }            
+        }
+
+        async Task ReceiveMessageAsync(StreamReader reader)
+        {
+
+            // считываем ответ в виде строки
+            string message = await reader.ReadLineAsync();
+            // если пустой ответ, ничего не выводим на консоль
+            if (!string.IsNullOrEmpty(message)) {
+                listBox1.BeginUpdate();
+                listBox1.Items.Add(message);
+                listBox1.EndUpdate();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text != null || textBox2.Text != "") SendMessageAsync(Writer);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Reader != null)
+            {
+                ReceiveMessageAsync(Reader);
+            }
+        }
+    }
+}
